16238b50c2c4090b1da4d0b9ff55b42724a59c46
Peter Lane <muffinspawn@gmail.com>
2019-07-04 11:08:30 -0500

    Added flag animation.

--- IndependenceDay/code.py
+++ IndependenceDay/code.py
@@ -1,49 +1,97 @@
+import os
 import time
 import math
+
 import board
+import displayio
 import neopixel
 import touchio
 
-NUM_PIXELS = 30                        # NeoPixel strip length (in pixels)
-NEOPIXEL_PIN = board.EXTERNAL_NEOPIXEL # Pin where NeoPixels are connected
-strip = neopixel.NeoPixel(NEOPIXEL_PIN, NUM_PIXELS, brightness=0.01, auto_write=False)
+class Animation:
+    # @abstractmethod
+    def step(self):
+        pass
 
-increase_brightness_pad = touchio.TouchIn(board.A5)
-decrease_brightness_pad = touchio.TouchIn(board.A2)
+class ScrollingRedWhitBlue(Animation):
+    RED = (255, 0, 0)
+    BLUE = (0, 0, 255)
+    WHITE = (255, 255, 255)
 
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-WHITE = (255, 255, 255)
+    def __init__(self, strip):
+        self.__strip = strip
 
-COLORS = [0]*NUM_PIXELS
-for index in range(10):
-    COLORS[index] = RED
-    COLORS[index+10] = WHITE
-    COLORS[index+20] = BLUE
+        self.COLORS = [0]*self.__strip.n
+        for index in range(10):
+            self.COLORS[index] = self.RED
+            self.COLORS[index+10] = self.WHITE
+            self.COLORS[index+20] = self.BLUE
 
-DELAY = 0.0
-BRIGHTNESS_INCREMENT = 0.01
+        self.__lead_pixel = 0
 
-lead_pixel = 0
-brightness = 0.01
-while True:
-    if increase_brightness_pad.value:
-        brightness += BRIGHTNESS_INCREMENT
-        if brightness > 1.0:
-            brightness = 1.0
-    elif decrease_brightness_pad.value:
-        brightness -= BRIGHTNESS_INCREMENT
-        if brightness < 0.0:
-            brightness = 0.0
-    if strip.brightness != brightness:
-        strip.brightness = brightness
+    def step(self):
+        for index in range(self.__strip.n):
+            pixel = self.__lead_pixel + index
+            if pixel >= self.__strip.n:
+                pixel = pixel - self.__strip.n
+            self.__strip[pixel] = self.COLORS[index]
+        self.__strip.show()
+        self.__lead_pixel += 1
+        if self.__lead_pixel >= self.__strip.n:
+            self.__lead_pixel = 0
 
-    for index in range(NUM_PIXELS):
-        pixel = lead_pixel + index
-        if pixel >= NUM_PIXELS:
-            pixel = pixel - NUM_PIXELS
-        strip[pixel] = COLORS[index]
-    strip.show()
-    lead_pixel += 1
-    if lead_pixel >= NUM_PIXELS:
-        lead_pixel = 0
+class WavingFlag(Animation):
+    def __init__(self, display):
+        self.__display = display
+        self.__sprite = displayio.Group()
+        self.__display.show(self.__sprite)
+        self.__tiles = []
+        filenames = [filename for filename in os.listdir('images/') if filename.endswith('.bmp')].sort()
+        for filename in filenames:
+            with open(filename, "rb") as file:
+                image = displayio.OnDiskBitmap(file)
+                tile = displayio.TileGrid(image, pixel_shader=displayio.ColorConverter())
+                self.__tiles.append(tile)
+        self.__index = 0
+        self.__lead_pixel = 0
+
+    def step(self):
+        self.__sprite.append(self.__tiles[self.__index])
+        self.__display.wait_for_frame()
+        self.__sprite.pop()
+        self.__index += 1
+        if self.__index >= len(self.__tiles):
+            self.__index = 0
+
+def main():
+    NUM_PIXELS = 30                        # NeoPixel strip length (in pixels)
+    NEOPIXEL_PIN = board.EXTERNAL_NEOPIXEL # Pin where NeoPixels are connected
+    strip = neopixel.NeoPixel(NEOPIXEL_PIN, NUM_PIXELS, brightness=0.01, auto_write=False)
+
+    board.DISPLAY.brightness = 1.0
+
+    increase_brightness_pad = touchio.TouchIn(board.A5)
+    decrease_brightness_pad = touchio.TouchIn(board.A2)
+
+    DELAY = 0.0
+    BRIGHTNESS_INCREMENT = 0.01
+
+    brightness = 0.01
+    strip_animation = ScrollingRedWhitBlue(strip)
+    display_animation = WavingFlag(board.DISPLAY)
+    while True:
+        if increase_brightness_pad.value:
+            brightness += BRIGHTNESS_INCREMENT
+            if brightness > 1.0:
+                brightness = 1.0
+        elif decrease_brightness_pad.value:
+            brightness -= BRIGHTNESS_INCREMENT
+            if brightness < 0.0:
+                brightness = 0.0
+        if strip.brightness != brightness:
+            strip.brightness = brightness
+
+        strip_animation.step()
+        display_animation.step()
+
+if __name__ == '__main__':
+    main()
